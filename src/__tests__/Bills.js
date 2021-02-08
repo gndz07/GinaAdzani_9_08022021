759c5fd6a458699da4c9792305059eaf38528645
Gina A <ginadzani@gmail.com>
2021-02-08 15:06:31 +0100

    initial commit

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,6 +1,13 @@
 import { screen } from "@testing-library/dom"
+import userEvent from '@testing-library/user-event'
+import Bills  from "../containers/Bills.js"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
+import VerticalLayout from './VerticalLayout.js'
+import { localStorageMock } from "../__mocks__/localStorage.js"
+import firebase from "../__mocks__/firebase"
+import { ROUTES } from "../constants/routes"
+
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -8,14 +15,112 @@ describe("Given I am connected as an employee", () => {
       const html = BillsUI({ data: []})
       document.body.innerHTML = html
       //to-do write expect expression
+      const billIcon = screen.getByTestId("icon-window")
+      expect(billIcon.classList.contains("active-icon")).toBeTruthy()
     })
     test("Then bills should be ordered from earliest to latest", () => {
       const html = BillsUI({ data: bills })
       document.body.innerHTML = html
-      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
+      const dates = screen.getAllByText(/^(19|20)\d\d[- \.](0[1-9]|1[012])[- \.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
+    })
+  })
+  describe('When I am on bills page but it is loading', () => {
+    test('Then, Loading page should be rendered', () => {
+      const html = BillsUI({ loading: true })
+      document.body.innerHTML = html
+      expect(screen.getAllByText('Loading...')).toBeTruthy()
+    })
+  })
+  describe('When I am on bills page but back-end send an error message', () => {
+    test('Then, Error page should be rendered', () => {
+      const html = BillsUI({ error: 'some error message' })
+      document.body.innerHTML = html
+      expect(screen.getAllByText('Erreur')).toBeTruthy()
+    })
+  })
+})
+
+describe('When I am connected as an employee and I am on the bills page', () => {
+  describe('When I click on the Make New Bill button', () => {
+    test('A new bill modal should open', () => {
+      Object.defineProperty(window, 'localStorage', {value: localStorageMock})
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const html = BillsUI({data: []})
+      document.body.innerHTML = html
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({pathname})
+      }
+      const firestore = null
+      const bill = new Bills({ document, onNavigate, firestore, localStorage: window.localstorage })
+
+      const handleClickNewBill = jest.fn(bill.handleClickNewBill)
+      const newBillIcon = screen.getByTestId('btn-new-bill')
+      newBillIcon.addEventListener('click', handleClickNewBill)
+      userEvent.click(newBillIcon)
+      expect(handleClickNewBill).toHaveBeenCalled()
+
+      const modal = screen.getByTestId('form-new-bill')
+      expect(modal).toBeTruthy()
+    })
+  })
+
+  describe('When I click on the eye icon', () => {
+    test('A modal should open', () => {
+      Object.defineProperty(window, 'localStorage', {value: localStorageMock})
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const html = BillsUI({ data: bills })
+      document.body.innerHTML = html
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({pathname})
+      }
+      const firestore = null
+      const bill = new Bills({ document, onNavigate, firestore, localStorage: window.localstorage })
+
+      const handleClickIconEye = jest.fn(bill.handleClickIconEye)
+      const eyeIcon = screen.getAllByTestId('icon-eye')
+      eye[0].addEventListener('click', handleClickIconEye(eye[0]))
+      userEvent.click(eye[0])
+      expect(handleClickIconEye).toHaveBeenCalled()
+
+      const modale = screen.getByTestId('modaleFileEmployee')
+      expect(modale).toBeTruthy()
+    })
+  })
+})
+
+//GET integration test
+describe("Given I am a user connected as an Employee", () => {
+  describe("When I navigate to bills page", () => {
+    test("fetches bills from mock API GET", async () => {
+       const getSpy = jest.spyOn(firebase, "get")
+       const bills = await firebase.get()
+       expect(getSpy).toHaveBeenCalledTimes(1)
+       expect(bills.data.length).toBe(4)
+    })
+    test("fetches bills from an API and fails with 404 message error", async () => {
+      firebase.get.mockImplementationOnce(() =>
+        Promise.reject(new Error("Erreur 404"))
+      )
+      const html = BillsUI({ error: "Erreur 404" })
+      document.body.innerHTML = html
+      const message = await screen.getByText(/Erreur 404/)
+      expect(message).toBeTruthy()
+    })
+    test("fetches messages from an API and fails with 500 message error", async () => {
+      firebase.get.mockImplementationOnce(() =>
+        Promise.reject(new Error("Erreur 500"))
+      )
+      const html = BillsUI({ error: "Erreur 500" })
+      document.body.innerHTML = html
+      const message = await screen.getByText(/Erreur 500/)
+      expect(message).toBeTruthy()
     })
   })
 })
--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,14 +1,55 @@
-import { screen } from "@testing-library/dom"
+import { fireEvent, screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
+import { localStorageMock } from "../__mocks__/localStorage.js"
+import firebase from "../__mocks__/firebase"
+import { ROUTES } from "../constants/routes"
 
 
-describe("Given I am connected as an employee", () => {
-  describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+describe('Given I am connected as an employee', () => {
+  describe('When I am on NewBill Page', () => {
+    test('When I choose a new file to upload, the file should be loaded and handled', () => {
+    	Object.defineProperty(window, 'localStorage', {value: localStorageMock})
+    		window.localStorage.setItem('user', JSON.stringify({
+    		type: 'Employee'
+    	}))
+    	const jsdomAlert = window.alert;  // remember the jsdom alert
+  		window.alert = () => {};
+     	const html = NewBillUI()
+     	document.body.innerHTML = html
+    	const onNavigate = (pathname) => {
+    		document.body.innerHTML = ROUTES({pathname})
+     	}
+     	const firestore = null
+     	const newBill = new NewBill({ document, onNavigate, firestore, localStorage: window.localStorage })
+    
+    	const handleChangeFile = jest.fn(newBill.handleChangeFile)
+     	const fileUpload = screen.getByTestId('file')
+     	fileUpload.addEventListener('change', handleChangeFile)
+     	fireEvent.change(fileUpload)
+     	expect(handleChangeFile).toHaveBeenCalled()
+    })
+    test('When I click on the submit button with the right input, my new bill should be submitted', () => {
+    	Object.defineProperty(window, 'localStorage', {value: localStorageMock})
+    		window.localStorage.setItem('user', JSON.stringify({
+    		type: 'Employee'
+    	}))
+     	const html = NewBillUI()
+     	document.body.innerHTML = html
+    	const onNavigate = (pathname) => {
+    		document.body.innerHTML = ROUTES({pathname})
+     	}
+     	const jsdomAlert = window.alert;  // remember the jsdom alert
+		window.alert = () => {};
+     	const firestore = null
+     	const newBill = new NewBill({ document, onNavigate, firestore, localStorage: window.localStorage })
+
+     	const handleSubmit = jest.fn(newBill.handleSubmit)
+     	const createBill = jest.fn(newBill.createBill)
+     	const submitNewBill = screen.getByTestId('form-new-bill')
+     	submitNewBill.addEventListener('submit', handleSubmit)
+     	fireEvent.submit(submitNewBill)
+     	expect(handleSubmit).toHaveBeenCalled()
     })
   })
 })
